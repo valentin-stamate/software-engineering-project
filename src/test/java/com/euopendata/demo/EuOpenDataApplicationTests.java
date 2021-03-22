0139182367e31cdc0015eb43c7faf861b4672a75
ValentinSt <stamatevalentin125@gmail.com>
2021-03-22 15:29:28 +0200

    backend initial commit

---
+++ src/test/java/com/euopendata/demo/EuOpenDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.euopendata.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuOpenDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
