68fe101ab43cb6c25c69d7cbcf55550528f7cbdb
Mihai Cristian-Laurentiu <79109977+LaurentiuMihai112@users.noreply.github.com>
2021-03-23 17:17:52 +0200

    Added EU Open Data related classes

---
+++ backend/src/main/java/com/bfourclass/euopendata/opendata/EUOpenDataClient.java
@@ -0,0 +1,17 @@
+package com.bfourclass.euopendata.opendata;
+
+public class EUOpenDataClient {
+    private String apiKey;
+
+    public EUOpenDataClient() {
+
+    }
+
+    /**
+     * TODO Get data about a city from EU Open Data
+     * Need a practical formatting for the data
+     */
+    public EUOpenData requestEUOpenData(String city) {
+        return null;
+    }
+}
