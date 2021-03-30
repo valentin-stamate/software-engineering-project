c93d5e77046a9ae58a0f5eb53af9e8cff66321f5
logofatuionut <logofatuionut@yahoo.com>
2021-03-30 13:16:01 +0300

    Added hashing algo

--- backend/src/main/java/com/bfourclass/euopendata/security/SimpleHashingAlgo.java
+++ backend/src/main/java/com/bfourclass/euopendata/security/SimpleHashingAlgo.java
@@ -1,9 +1,29 @@
 package com.bfourclass.euopendata.security;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class SimpleHashingAlgo implements HashingAlgo {
 
     @Override
     public String hash(String text) {
-        return null;
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            md.update(salt);
+            byte[] bytes = md.digest(text.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return generatedPassword;
     }
 }
