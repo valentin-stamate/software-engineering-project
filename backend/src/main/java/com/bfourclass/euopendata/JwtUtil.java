58348e1b76052925c9745373dbf906006b588f74
lucimaster100 <74505266+lucimaster100@users.noreply.github.com>
2021-03-31 19:10:47 +0300

    Create JwtUtil.java

---
+++ backend/src/main/java/com/bfourclass/euopendata/JwtUtil.java
@@ -0,0 +1,35 @@
+@Service
+public class JwtUtil{
+    private String SECRET_KEY= "secret";
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+    public Date extractExpiration(String token){
+        return extractClaim(token,Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Claims extractAllClaims(String token){
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+    private Boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+    public String generateToken(User user){
+        Map <String, Object> claims = new HashMap<>();
+        return createToken(claims,user.getUsername());
+    }
+    private String createToken(Map<String,Object> claims,String subject){
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+ 1000*60*60*12))
+                .signWidth(SignatureAlgorithm.HS256,SECRET_KEY).compact();
+    }
+    public Boolean validateToken(String token, User user){
+        final String username=extractUsername(token);
+        return (username.equals(user.getUsername()) && !isTokenExpired(token));
+    }
+}
