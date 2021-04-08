51e4fa5ee1a453a1d4318812fa286f0a68bac1f3
logofatuionut <logofatuionut@yahoo.com>
2021-04-08 23:24:23 +0300

    Added jwt token generation

--- backend/src/main/java/com/bfourclass/euopendata/user/auth/InMemorySecurityContext.java
+++ backend/src/main/java/com/bfourclass/euopendata/user/auth/InMemorySecurityContext.java
@@ -1,18 +1,45 @@
 package com.bfourclass.euopendata.user.auth;
 
+import com.bfourclass.euopendata.user.User;
+import com.bfourclass.euopendata.user.UserService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
 /**
  * Autentificare simpla, in-memory, nu in baza de date
  */
 public class InMemorySecurityContext implements SecurityContext {
+    private String SECRET_KEY= "secret";
 
-    @Override
-    public boolean isValid(String token) {
-        return false;
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
     }
 
     @Override
-    public String generateToken() {
-        return null;
+    public boolean isValid(String token, UserService userService) {
+        String username = extractUsername(token);
+        return userService.userExists(username);
+    }
+
+    @Override
+    public String generateToken(String username){
+        return Jwts.builder().setSubject(username).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+ 1000*60*60*12))
+                .signWith(SignatureAlgorithm.HS256,SECRET_KEY).compact();
     }
 
     @Override
--- backend/src/main/java/com/bfourclass/euopendata/user/auth/SecurityContext.java
+++ backend/src/main/java/com/bfourclass/euopendata/user/auth/SecurityContext.java
@@ -1,10 +1,12 @@
 package com.bfourclass.euopendata.user.auth;
 
+import com.bfourclass.euopendata.user.UserService;
+
 public interface SecurityContext {
 
-    boolean isValid(String token);
+    boolean isValid(String token, UserService userService);
 
-    String generateToken();
+    String generateToken(String username);
 
     void authenticateUser(String username, String token);
 
