efec48a85ef8ede2ada925e89d90435cb48a475a
Stefan Pezamosca <stefan2014pe@gmail.com>
2021-04-13 22:51:48 +0300

    bug fix

--- frontend/Web plugin/SmartBooking-Plugin/src/bg/background.js
+++ frontend/Web plugin/SmartBooking-Plugin/src/bg/background.js
@@ -1,5 +1,4 @@
 var locations = []
-
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ "message": locations });
 });
@@ -7,6 +6,9 @@ chrome.runtime.onInstalled.addListener(() => {
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
+	chrome.storage.sync.get('message', value => {
+		locations = value.message;
+	});
   	console.log(request);
 	if (!locations.includes(request)){
 		locations.push(request);
