1236cb87f4272711f75c4600a0204c34b0818a9f
Stefan Pezamosca <stefan2014pe@gmail.com>
2021-04-22 10:05:04 +0300

    small change

--- frontend/Web plugin/SmartBooking-Plugin/src/inject/inject.js
+++ frontend/Web plugin/SmartBooking-Plugin/src/inject/inject.js
@@ -10,6 +10,11 @@ function getName() {
     return name.trim().replace("\n", "");
 }
 
+var hotelName = getName();
+var hotelAdress = getHotelAdress();
+console.log("Hotel name = " + hotelName);
+console.log("Hotel adress = " + hotelAdress);
+
 function addPopupHtml() {
     let popup_str =
         `<div id="main-popup">
@@ -27,125 +32,77 @@ function addPopupHtml() {
 }
 addPopupHtml();
 
-var hotelName = getName();
-var hotelAdress = getHotelAdress();
-console.log("Hotel name = " + hotelName);
-console.log("Hotel adress = " + hotelAdress);
-
-function addStatistics(_stats) {
-    let stats_div = document.getElementById("statistics-container");
-    stats_div.innerHTML = "";
-
-    let hotelInfoSection =
-        `<section id="hotel-info">
-	<h3>${_stats.hotel.hotelName}</h3>
-	<h5>${_stats.hotel.locationName}</h5>
-</section>`;
-    let covidInfoSection =
-        `<section id="covid-info">
-	<h4>Covid information:</h4>
-	<ul id="covid_info_list">
-	</ul>
-</section>`;
-    let weatherInfoSection = `
-    <h4>Weather information:</h4>
-    <div id="weather_info_list">
-    </div>
-    `
-
-    let weatherCard = `
-    <div class="weather_card">
-        <h2 class="weather_card__day">Monday</h2>
-        <div class="weather_card__info_main">
-            <h3 class="weather_card__description">Cloudy</h3>
-            <div>
-                <h3 class="weather_card__description weather_card__info">Wind 10km/h</h3>
-                <h3 class="weather_card__description weather_card__info">Precip 0%</h3>
-                <h3 class="weather_card__description weather_card__info">Air pollution 10%</h3>
-                <h3 class="weather_card__description weather_card__info">Air pressure 10 bar</h3>
-            </div>
-        </div>
-        <div class="weather_card__main">
-            <div class="weather_card__sky">
-                <img src="http://openweathermap.org/img/wn/10d@2x.png">
-            </div>
-            <h1 class="weather_card__avg_temp">23°</h1>
-        </div>
-        <table class="weather_card__data">
-            <tr class="weather_card__hours">
-                <td></td>
-                <td>3:00</td>
-                <td>6:00</td>
-                <td>9:00</td>
-                <td>12:00</td>
-            </tr>
-            <tr class="weather_card__temp_am">
-                <td>AM</td>
-                <td>30°</td>
-                <td>34°</td>
-                <td>36°</td>
-                <td>34°</td>
-            </tr>
-            <tr class="weather_card__temp_pm">
-                <td>PM</td>
-                <td>17°</td>
-                <td>22°</td>
-                <td>19°</td>
-                <td>23°</td>
-            </tr>
-        </table>
-    `;
-
-    /*
-        `<section id="weather-info">
-	<h4>Weather information:</h4>
-	<ul id="weather_info_list">
-		<li>Coordonates - { Latitude: ${_stats.weather.coord.lat}, Longitude: ${_stats.weather.coord.lon} }</li>
-		<li>Temperature data:
-			<ul>
-				<li>Avg. temperature: ${_stats.weather.main.temp}&#186;C</li>
-				<li>Max temperature: ${_stats.weather.main.temp_max}&#186;C</li>
-				<li>Min temperature: ${_stats.weather.main.temp_min}&#186;C</li>
-				<li>Feels like: ${_stats.weather.main.feels_like}&#186;C</li>
-			</ul>
-		</li>
-	</ul>
-</section>`;
-    let airPolutionSection =
-        `<section id="air-pollution">
-	<h4>Air pollution information:</h4>
-	<ul id="air_info_list">
-		<li>Air humidity: ${_stats.weather.main.humidity}</li>
-		<li>Air pressure: ${_stats.weather.main.pressure}</li>
-		<li>Air quality index: ${_stats.airPollution.airQualityIndex}</li>
-		<li>Air particle matter (up to 10 micrometers): ${_stats.airPollution.pm10Value}</li>
-	</ul>
-</section>`;
-*/
-    stats_div.innerHTML = hotelInfoSection + "\n" + covidInfoSection + "\n" +
-        "\n" + weatherInfoSection + "\n"; //+ airPolutionSection;
-
-    let weatherInfo = document.getElementById("weather_info_list");
-
-    // aici inserez de 5 ori weather_card
-    for (i = 0; i < 5; i++) {
-        weatherInfo.innerHTML += weatherCard;
-    }
-}
-
-// Experimental function
 async function getStatistics() {
     console.log("requesting statistics");
     var _data = {
         sendStatistics: true,
         hotelLocation: destination,
     }
-    chrome.runtime.sendMessage(_data, function(response) {
+    chrome.runtime.sendMessage(_data, function (response) {
         console.log(response);
-        //addStatistics(response);
+        addStatistics(response);
     });
 }
 getStatistics();
+
+function addStatistics(_stats) {
+    let stats_div = document.getElementById("statistics-container");
+    stats_div.innerHTML = `<section id="weather-statistics"></section>`;
+
+    addForecastItem("");
+    addForecastItem("");
+    addForecastItem("");
+    addForecastItem("");
+}
+
+async function addForecastItem(_forecast){
+    let weatherCard =
+        `<div class="weather_card">
+    <h2 class="weather_card__day">Monday</h2>
+    <div class="weather_card__info_main">
+        <h3 class="weather_card__description">Cloudy</h3>
+        <div>
+            <h3 class="weather_card__description weather_card__info">Wind 10km/h</h3>
+            <h3 class="weather_card__description weather_card__info">Precip 0%</h3>
+            <h3 class="weather_card__description weather_card__info">Air pollution 10%</h3>
+            <h3 class="weather_card__description weather_card__info">Air pressure 10 bar</h3>
+        </div>
+    </div>
+    <div class="weather_card__main">
+        <div class="weather_card__sky">
+            <img src="http://openweathermap.org/img/wn/10d@2x.png">
+        </div>
+        <h1 class="weather_card__avg_temp">23°</h1>
+    </div>
+    <table class="weather_card__data">
+        <tr class="weather_card__hours">
+            <td></td>
+            <td>3:00</td>
+            <td>6:00</td>
+            <td>9:00</td>
+            <td>12:00</td>
+        </tr>
+        <tr class="weather_card__temp_am">
+            <td>AM</td>
+            <td>30°</td>
+            <td>34°</td>
+            <td>36°</td>
+            <td>34°</td>
+        </tr>
+        <tr class="weather_card__temp_pm">
+            <td>PM</td>
+            <td>17°</td>
+            <td>22°</td>
+            <td>19°</td>
+            <td>23°</td>
+        </tr>
+    </table>
+</div>`;
+
+    let weatherInfo = document.getElementById("weather-statistics");
+
+    weatherInfo.innerHTML += weatherCard;
+}
 
 var show = true;
 
@@ -171,7 +128,7 @@ function sendPreferences() {
         hotelLocation: destination,
         hotelPath: hotel_path
     }
-    chrome.runtime.sendMessage(_data, function(response) {
+    chrome.runtime.sendMessage(_data, function (response) {
         console.log(JSON.stringify(response));
     });
 }
