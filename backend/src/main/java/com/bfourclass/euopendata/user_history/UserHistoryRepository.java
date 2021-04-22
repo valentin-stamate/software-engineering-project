c3a34af6d2d70aa5d4c3d740064392a0e9d6da49
Alin Ursu <33602125+ursualin7890@users.noreply.github.com>
2021-04-22 16:46:31 +0300

    Added retrieving user history

--- backend/src/main/java/com/bfourclass/euopendata/user_history/UserHistoryRepository.java
+++ backend/src/main/java/com/bfourclass/euopendata/user_history/UserHistoryRepository.java
@@ -5,6 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserHistoryRepository extends JpaRepository<UserHistoryModel, Long> {
+public interface UserHistoryRepository extends JpaRepository<UserHistoryModel, Long>
+{
+    @Query("SELECT h FROM UserHistoryModel h")
+    List<UserHistoryModel> getAll();
 }
