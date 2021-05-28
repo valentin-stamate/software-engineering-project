0534bb4c27f64ccfa318f279d756833b0a74a7f9
ValentinSt <stamatevalentin125@gmail.com>
2021-05-28 09:02:05 +0300

    fixing google search bug

--- backend/src/main/java/com/bfourclass/euopendata/secrets/Secrets.java
+++ backend/src/main/java/com/bfourclass/euopendata/secrets/Secrets.java
@@ -2,6 +2,6 @@ package com.bfourclass.euopendata.secrets;
 
 public abstract class Secrets {
     public final static String OPEN_WEATHER_API_KEY = "7a7ffe4e410058a41e571c2507d2f767";
-    public final static String GOOGLE_CUSTOM_SEARCH_API_KEY = "AIzaSyCEyRbroYdpfbNjFFkARdJnJ4g0i8wuXz0";
+    public final static String GOOGLE_CUSTOM_SEARCH_API_KEY = "AIzaSyAz2h217E56I2V6gEwIuqcFPvUBwqZzOtk";
     public final static String WEATHER_BIT_API_KEY = "a7befe2b657340668341e9efc0746fc0";
 }
